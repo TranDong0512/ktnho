a0c699a5ccb00a1f4bc419e04fb201160b235171
TranDong0512 <tdong0512@gmail.com>
2022-08-09 11:20:46 +0700

    ktnho

---
+++ main.js
@@ -0,0 +1,11 @@
+function clickon() {
+   let so1= +document.getElementById("a").value
+    let cv =4*a;
+   let dt = a**2;
+   document.getElementById("text").innerHTML="chu vi là " +cv+ "diện tích là "+ dt;
+}
+function clickon1() {
+    let so1= +document.getElementById("a").value
+    let so2= +document.getElementById("b").value
+    document.getElementById("text").innerHTML="a bình "+ a**2+ " b bình " + b**2
+}
